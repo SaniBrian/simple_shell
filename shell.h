05f8c3e93f803046a068978c2dde06193a679ada
SaniBrian <sanibrian19@gmail.com>
2023-05-23 11:47:58 -0700

    Header file update

--- shell.h
+++ shell.h
@@ -32,6 +32,12 @@ char *_strcpy(char *s1, char *s2);
 void set_env_loop(char *new_var, char **tmp, char **args, char *prog);
 void unset_env(char **args, char *prog);
 void unset_env_loop(char **tmp, char **args, char *prog);
+void signal_handler(int signal);
+void cd_dir(char **args, char *prog);
+void chng_env(char *var, char *v_name, char *prog);
+void chng_env_loop(char *new_var, char *v_name, char **tmp, char *prog);
+void cd_absolute(char **args, char *pwd, char *prog);
+int err_check(int err, char *prog);
 
 
 /**BUILT_INS STRUCT*/
